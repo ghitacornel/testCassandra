b2e9a0f050e6de2a5c3c066405eaa809a32e54fd
cornel <ghita.cornel@gmail.com>
2022-10-22 00:04:07 +0300

    xxx

--- src/main/java/cassandra/model/Book.java
+++ src/main/java/cassandra/model/Book.java
@@ -1,40 +1,16 @@
 package cassandra.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
 public class Book {
 
     private UUID id;
     private String title;
     private String subject;
 
-    public Book(UUID id, String title, String subject) {
-        this.id = id;
-        this.title = title;
-        this.subject = subject;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
 }
