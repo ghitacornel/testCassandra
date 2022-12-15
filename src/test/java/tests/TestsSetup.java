8ec705af24bc1ebefe26ccede853387e9dfe3298
cornel <ghita.cornel@gmail.com>
2022-12-15 13:35:50 +0200

    better

--- src/test/java/tests/TestsSetup.java
+++ src/test/java/tests/TestsSetup.java
@@ -3,7 +3,10 @@ package tests;
 import cassandra.CassandraConnector;
 import cassandra.repository.KeySpaceRepository;
 import com.datastax.driver.core.Session;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import static cassandra.repository.KeySpaceRepository.KEYSPACE_NAME;
 
 public abstract class TestsSetup {
 
@@ -17,6 +20,7 @@ public abstract class TestsSetup {
         client.connect();
         session = client.getSession();
         schemaRepository = new KeySpaceRepository(session);
+        schemaRepository.createKeyspace(KEYSPACE_NAME, "SimpleStrategy", 1);
     }
 
     @AfterClass
